5ea85095dc181fe0d76e0e49ae792cba6586590a
jiahua <jiahua.xu@outlook.com>
2019-05-08 18:22:09 +0800

    tmp

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math/rand"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
+
+	term "github.com/nsf/termbox-go"
 )
 
 func nextUnknownWord(words []*Word) chan *Word {
@@ -23,23 +29,103 @@ func nextUnknownWord(words []*Word) chan *Word {
 	return wordChan
 }
 
-func main() {
-	// space: next word
-	// k: i know it
-	// i: invalid word
+func dumpPeriodically(words []*Word, done chan bool) {
+	go func() {
+		ticker := time.NewTicker(5 * time.Second)
+		defer ticker.Stop()
 
-	// as a param
-	filename := "word_freq_sample.csv"
+		var gracefulStop = make(chan os.Signal)
+		signal.Notify(gracefulStop, syscall.SIGTERM)
+		signal.Notify(gracefulStop, syscall.SIGINT)
+
+		for {
+			select {
+			case <-done:
+				return
+			case t := <-ticker.C:
+				_ = t
+				// TODO: dump words if has change
+			case <-gracefulStop:
+				// fmt.Println("Catch CTRL-C")
+				Dump(filename, words)
+				os.Exit(0)
+			}
+		}
+	}()
+}
+
+var filename string
+
+func init() {
+	const (
+		defaultFilename = "word_freq_sample.csv"
+	)
+	flag.StringVar(&filename, "csv_file", defaultFilename, "rem-word -csv_file="+defaultFilename)
+	flag.Parse()
+}
+
+const usage = `
+	space: next word
+	k: i know it
+	i: invalid word
+	t: translate it
+	CTRL-C: quit
+`
+
+func reset() {
+	term.Sync() // cosmestic purpose
+}
+
+func main() {
 	words, err := Read(filename)
 	if err != nil {
 		panic(err)
 	}
 
+	err = term.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer term.Close()
+
+	done := make(chan bool)
+	dumpPeriodically(words, done)
+
 	wordChan := nextUnknownWord(words)
 
+	// TODO: in a endless loop
 	for word := range wordChan {
+		reset()
 		fmt.Println(word.Token)
+		fmt.Println(usage)
+
+	keyPressListenerLoop:
+		for {
+			switch ev := term.PollEvent(); ev.Type {
+			case term.EventKey:
+				switch ev.Key {
+				case term.KeyCtrlC:
+					// TODO: handle
+					os.Exit(0)
+				case term.KeySpace:
+					break keyPressListenerLoop
+				default:
+					// we only want to read a single character or one key pressed event
+					// fmt.Println("ASCII : ", ev.Ch)
+					switch ev.Ch {
+					case 'k':
+						fmt.Println("mark it known")
+					case 'd':
+						fmt.Println("delete it")
+					case 't':
+						fmt.Println("translate it")
+					}
+				}
+			case term.EventError:
+				panic(ev.Err)
+			}
+		}
 	}
 
-	Dump(filename, words)
+	done <- true
 }
